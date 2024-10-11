0ac7bdc6d36b7e9cafbc0e1a3eb47643f7b036fb
juancruzmarzetti <naujmarz@gmail.com>
2024-10-10 22:51:41 -0300

    removing-tries-of-free-tier

--- db.tf
+++ db.tf
@@ -9,6 +9,7 @@ resource "aws_db_instance" "default" {
   password = var.db_password
   parameter_group_name = "default.mysql8.0"
   skip_final_snapshot = true
+  publicly_accessible = true
   port = var.database_port
   identifier = "mydb"
   vpc_security_group_ids = [aws_security_group.allow_custom_tcp.id]
--- sg.tf
+++ sg.tf
@@ -38,13 +38,6 @@ resource "aws_security_group" "docker" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress {
-    protocol    = "tcp"
-    from_port   = 3306
-    to_port     = 3306
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
   egress {
     protocol    = -1
     from_port   = 0
